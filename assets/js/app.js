02ed37d2c771f05126a6a956e69564c9a29b251b
iCarlo <62226010+iCarlo@users.noreply.github.com>
2020-11-11 13:44:41 +0800

    todo-main structure fixed

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,14 +5,13 @@ const todoList = document.querySelector('.todo-list')
 
 addBtn.addEventListener('click', () => {
     if (userInput.value) {
-        const todoItem = userInput.value
-        const listItem = `<li>
-        <div class="todo-item"><span>${todoItem}</span><div class="todo-actions"><button class="todo-edit"><i class="fas fa-edit"></i></button><button class="todo-delete"><i class="fas fa-trash-alt"></i></button></div></div>
-    </li>`
+        const newItem = userInput.value
+        const todoItem = `<li><div class="todo-item"><span>${newItem}</span><div class="todo-actions"><button class="todo-edit"><i class="fas fa-edit"></i></button><button class="todo-delete"><i class="fas fa-trash-alt"></i></button></div></div></li>`
+
 
         // afterbegin - append as first child
         // beforeend - append as last child
-        todoList.insertAdjacentHTML("afterbegin", listItem)
+        todoList.insertAdjacentHTML("afterbegin", todoItem)
         userInput.value = null
     }
 
