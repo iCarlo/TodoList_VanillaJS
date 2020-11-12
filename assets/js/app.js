dc0284c1ff37e64d3f14bf2c935e523eb4c5acdc
iCarlo <62226010+iCarlo@users.noreply.github.com>
2020-11-12 14:04:12 +0800

    delete todo function done

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,17 +3,39 @@ const addBtn = document.querySelector('#add-btn')
 const todoList = document.querySelector('.todo-list')
 
 
-addBtn.addEventListener('click', () => {
+
+addBtn.addEventListener('click', (e) => {
+    e.preventDefault()
     if (userInput.value) {
         const newItem = userInput.value
-        const todoItem = `<li><div class="todo-item"><span>${newItem}</span><div class="todo-actions"><button class="todo-edit"><i class="fas fa-edit"></i></button><button class="todo-delete"><i class="fas fa-trash-alt"></i></button></div></div></li>`
+        const todoItem = `<div class="todo-item"><span>${newItem}</span><div class="todo-actions"><button data-todo=${newItem} class="todo-edit" type="button"><i class="fas fa-edit"></i></button><button data-todo=${newItem} class="todo-delete" type="button"><i class="fas fa-trash-alt"></i></button></div></div>`
 
+        const listItem = document.createElement('li')
+        listItem.innerHTML = todoItem
 
         // afterbegin - append as first child
         // beforeend - append as last child
         todoList.insertAdjacentHTML("afterbegin", todoItem)
         userInput.value = null
-    }
 
+    }
 })
 
+
+todoList.addEventListener('click', (e) => {
+    if (e.target.className === "todo-edit") {
+        editTodo(e)
+    } else if (e.target.className === "todo-delete") {
+        deleteTodo(e)
+    }
+})
+
+
+const deleteTodo = (e) => {
+    const query = e.target.getAttribute('data-todo')
+    const todos = document.getElementsByClassName('todo-item')
+    const deleteItem = Array.from(todos).find(x => x.innerText === query)
+    deleteItem.remove()
+
+
+}
